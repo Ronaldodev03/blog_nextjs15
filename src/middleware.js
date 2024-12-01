e453b5378a4b0055a027af4183d4f65af6df0e45
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-01 01:09:43 -0400

    Add the create post page UI and protect it as an admin

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  if (!userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn();
+  }
+});
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
